86d922190fa6ff2101143b05afdb9481b005979b
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-07-23 05:31:20 -0400

    Made a new project , Solution exists that is an ASP.Net Web Project.
    Also made a ToDo class  with the Done, Name and EntryDate Properties with limit of Name field to 255 characters  .
    Also EntryDate property is automatically set to the current date and time.

---
+++ Data/ToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationToDo.Data
+{
+    public class ToDo
+    {
+        public Boolean Done { get; set; }
+
+        [StringLength(255, MinimumLength = 5, ErrorMessage = "This needs to be longer.")]
+        public string Name { get; set; }
+
+        [Key]
+
+
+        public DateTime EntryDate { get; set; } = DateTime.Now;
+    }
+}
